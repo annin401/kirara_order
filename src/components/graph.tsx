3435e19e40433fb26e6246e1bbaf76530bae2237
annin401 <annin401@gmail.com>
2020-04-10 15:32:15 +0900

    feat: サイズを横幅を自動で調節できるようにした

--- src/components/graph.tsx
+++ src/components/graph.tsx
@@ -2,7 +2,13 @@ import React from 'react';
 import '../styles/tailwind.css';
 
 import moment, { Moment } from 'moment';
-import { VictoryChart, VictoryAxis, VictoryContainer } from 'victory';
+import {
+  VictoryChart,
+  VictoryAxis,
+  VictoryZoomContainer,
+  VictoryLine,
+  VictoryTheme,
+} from 'victory';
 
 // issue の意味は「月号(3月号とかの月号)」
 function makeHorizontalTickData(oldestIssue: Moment, latestIssue: Moment) {
@@ -36,24 +42,102 @@ function makeHorizontalTickData(oldestIssue: Moment, latestIssue: Moment) {
   return tickArray;
 }
 
+function makeVirticalTickData(worstOrder: number) {
+  let tickArray = [];
+  // 間違って負の数が来た時でも正の数に直す
+  worstOrder = Math.abs(worstOrder);
+
+  for (let i = 0; i <= worstOrder; ++i) {
+    // グラフの縦軸の目盛りが原点から見て降順になるようにするため
+    // 負の数の配列にしている
+    tickArray.push(-i);
+  }
+  return tickArray;
+}
+
+type productionOrderArray = {
+  issue: string;
+  order: number;
+  isCover: boolean;
+  isColor: boolean;
+  isDouble: boolean;
+  isGuest: boolean; //　NOTE: 現在の構想ではゲストの情報はいらないためいずれ消す
+}[];
+
+type zoomDomain = {
+  x: [Date, Date];
+  y: [number, number];
+};
+
 type GraphProps = {};
 
 const Graph = (props: GraphProps) => {
   const {} = props;
 
-  const latestIssue = moment('2017-12'); // NOTE: ハードコーティング、仮
-  const oldestIssue = moment('2017-1'); // NOTE: ハードコーティング、仮
+  const [POArray, setPOArray] = React.useState<productionOrderArray>([]); // NOTE: 仮
+  const [zoomDomain, setZoomDomain] = React.useState<zoomDomain>({
+    x: [new Date(2019, 0), new Date(2019, 11)], // NOTE: ハードコーティング
+    y: [-30, 0], // NOTE: ハードコーティング
+  });
 
-  const virticalTickMaxNumber = 30; // NOTE: ハードコーティング、仮
   const graphHeight = 600; // NOTE: ハードコーティング、仮
   const graphWidth = 370; // NOTE: ハードコーティング、仮
+  const worstOrder = 30; // 縦軸で一番したの目盛りの値、正の数で指定
+
+  React.useEffect(() => {
+    const title = 'いのち短し善せよ乙女'; // NOTE: ハードコーティング
+    import(`../../static/${title}.json`).then(json => {
+      const orderArray = json.default.list;
+      setPOArray(orderArray);
+
+      setZoomDomain({
+        x: [
+          moment(orderArray[orderArray.length - 1].issue)
+            .add(-11, 'months') // NOTE: 11ヶ月のところがハードコーティング, 横幅によって変える
+            .toDate(),
+          moment(orderArray[orderArray.length - 1].issue).toDate(),
+        ],
+        y: [-worstOrder, 0], // マイナスにするのを忘れない！
+      });
+    });
+  }, []);
+
+  // NOTE: propsからくるオブジェクトの構造次第でこの関数は壊れます
+  // あとで書き換える前提。
+  const getOldestIssue = () => {
+    if (typeof POArray !== 'undefined' && POArray.length !== 0) {
+      return moment(POArray[0].issue);
+    } else {
+      const defaultOldestIssue = moment('2019-1'); // NOTE: ハードコーティング
+      return defaultOldestIssue;
+    }
+  };
+  const getLatestIssue = () => {
+    if (typeof POArray !== 'undefined' && POArray.length !== 0) {
+      return moment(POArray[POArray.length - 1].issue);
+    } else {
+      const defaultLatestIssue = moment('2019-12'); // NOTE: ハードコーティング
+      return defaultLatestIssue;
+    }
+  };
 
   return (
     <VictoryChart
       // デフォルトでは横幅を取れるだけ取ろうとするので、staticにする
-      containerComponent={<VictoryContainer responsive={false} />}
+      containerComponent={
+        <VictoryZoomContainer
+          zoomDomain={{
+            x: zoomDomain.x,
+            y: zoomDomain.y,
+          }}
+          allowZoom={false}
+          zoomDimension='x'
+          responsive={false}
+        />
+      }
       height={graphHeight}
       width={graphWidth}
+      theme={VictoryTheme.material}
     >
       <VictoryAxis
         /* 横軸 */
@@ -62,10 +146,10 @@ const Graph = (props: GraphProps) => {
         // 軸の位置を指定
         orientation='bottom'
         // 目盛りとして使用するデータを渡す
-        tickValues={makeHorizontalTickData(oldestIssue, latestIssue)}
+        tickValues={makeHorizontalTickData(getOldestIssue(), getLatestIssue())}
         // 目盛りのフォーマットを指定
         // ticlValuesで渡した配列の要素が一つづつ引数に渡される
-        tickFormat={issue => {
+        tickFormat={(issue: Date) => {
           // 毎年の1月に年の情報を表示する
           if (issue.getMonth() === 0) {
             // TODO: 年の情報を表示する、ただしデザインを考える
@@ -77,15 +161,21 @@ const Graph = (props: GraphProps) => {
       />
       <VictoryAxis
         /* 縦軸 */
+        // 通常VictoryAxisでは、目盛りの値の大きさを原点から見て降順に配置することができない
+        // (直交座標のようにしか配置できない)。そのため、直交座標でいうと第四象限に当たる場所を
+        // 描画に使うことにした。
+        // そのため、縦軸の目盛りはデータ上は負の数であるが、表示では絶対値を表示している。
         dependentAxis
-        tickCount={virticalTickMaxNumber}
-        // 縦軸の目盛りの値の範囲を指定
-        // グラフの縦軸の目盛りが原点から見て降順になるようにするため
-        // 負の数から0の範囲を描画するようにする
-        domain={[-virticalTickMaxNumber, 0]}
+        tickValues={makeVirticalTickData(worstOrder)}
         // 目盛りのフォーマットを指定
-        // domainに負の数を渡しているため、それを正の数として表示
-        tickFormat={order => Math.abs(order)}
+        // tickValuesに負の数を渡しているため、それを正の数として表示
+        tickFormat={(order: number) => Math.abs(order)}
+      />
+      <VictoryLine // 仮
+        data={POArray.map(value => ({
+          x: moment(value.issue).toDate(),
+          y: -value.order,
+        }))}
       />
     </VictoryChart>
   );
