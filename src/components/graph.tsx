70de5a9c86d8a658a58c7b8ff7580ee608d7ed72
annin401 <annin401@gmail.com>
2020-04-11 11:16:31 +0900

    faet: jsonが読み込み終わっていないときはjsonを読み込まないようにした

--- src/components/graph.tsx
+++ src/components/graph.tsx
@@ -137,62 +137,71 @@ const Graph = (props: GraphProps) => {
   };
 
   return (
-    <VictoryChart
-      // デフォルトでは横幅を取れるだけ取ろうとするので、staticにする
-      containerComponent={
-        <VictoryZoomContainer
-          zoomDomain={{
-            x: zoomDomain.x,
-            y: zoomDomain.y,
-          }}
-          allowZoom={false}
-          zoomDimension='x'
-          responsive={false}
-        />
-      }
-      height={graphHeight}
-      width={graphWidth}
-      theme={VictoryTheme.material}
-    >
-      <VictoryAxis
-        /* 横軸 */
-        // 目盛りで使用するデータ型の指定
-        scale='time'
-        // 軸の位置を指定
-        orientation='bottom'
-        // 目盛りとして使用するデータを渡す
-        tickValues={makeHorizontalTickData(getOldestIssue(), getLatestIssue())}
-        // 目盛りのフォーマットを指定
-        // ticlValuesで渡した配列の要素が一つづつ引数に渡される
-        tickFormat={(issue: Date) => {
-          // 毎年の1月に年の情報を表示する
-          if (issue.getMonth() === 0) {
-            // TODO: 年の情報を表示する、ただしデザインを考える
-            return `${issue.getMonth() + 1} \n ${issue.getFullYear()}`;
-          } else {
-            return `${issue.getMonth() + 1}`;
+    <>
+      {comicTitleArray.length === 0 ? (
+        <p>Looding...</p>
+      ) : (
+        <VictoryChart
+          // デフォルトでは横幅を取れるだけ取ろうとするので、staticにする
+          containerComponent={
+            <VictoryZoomContainer
+              zoomDomain={{
+                x: zoomDomain.x,
+                y: zoomDomain.y,
+              }}
+              allowZoom={false}
+              zoomDimension='x'
+              responsive={false}
+            />
           }
-        }}
-      />
-      <VictoryAxis
-        /* 縦軸 */
-        // 通常VictoryAxisでは、目盛りの値の大きさを原点から見て降順に配置することができない
-        // (直交座標のようにしか配置できない)。そのため、直交座標でいうと第四象限に当たる場所を
-        // 描画に使うことにした。
-        // そのため、縦軸の目盛りはデータ上は負の数であるが、表示では絶対値を表示している。
-        dependentAxis
-        tickValues={makeVirticalTickData(worstOrder)}
-        // 目盛りのフォーマットを指定
-        // tickValuesに負の数を渡しているため、それを正の数として表示
-        tickFormat={(order: number) => Math.abs(order)}
-      />
-      <VictoryLine // 仮
-        data={POArray.map(value => ({
-          x: moment(value.issue).toDate(),
-          y: -value.order,
-        }))}
-      />
-    </VictoryChart>
+          height={graphHeight}
+          width={graphWidth}
+          theme={VictoryTheme.material}
+        >
+          <VictoryAxis
+            /* 横軸 */
+            // 目盛りで使用するデータ型の指定
+            scale='time'
+            // 軸の位置を指定
+            orientation='bottom'
+            // 目盛りとして使用するデータを渡す
+            tickValues={makeHorizontalTickData(
+              getOldestIssue(),
+              getLatestIssue()
+            )}
+            // 目盛りのフォーマットを指定
+            // ticlValuesで渡した配列の要素が一つづつ引数に渡される
+            tickFormat={(issue: Date) => {
+              // 毎年の1月に年の情報を表示する
+              if (issue.getMonth() === 0) {
+                // TODO: 年の情報を表示する、ただしデザインを考える
+                return `${issue.getMonth() + 1} \n ${issue.getFullYear()}`;
+              } else {
+                return `${issue.getMonth() + 1}`;
+              }
+            }}
+          />
+          <VictoryAxis
+            /* 縦軸 */
+            // 通常VictoryAxisでは、目盛りの値の大きさを原点から見て降順に配置することができない
+            // (直交座標のようにしか配置できない)。そのため、直交座標でいうと第四象限に当たる場所を
+            // 描画に使うことにした。
+            // そのため、縦軸の目盛りはデータ上は負の数であるが、表示では絶対値を表示している。
+            dependentAxis
+            tickValues={makeVirticalTickData(worstOrder)}
+            // 目盛りのフォーマットを指定
+            // tickValuesに負の数を渡しているため、それを正の数として表示
+            tickFormat={(order: number) => Math.abs(order)}
+          />
+          <VictoryLine // 仮
+            data={POArray.map(value => ({
+              x: moment(value.issue).toDate(),
+              y: -value.order,
+            }))}
+          />
+        </VictoryChart>
+      )}
+    </>
   );
 };
 
